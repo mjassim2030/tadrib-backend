1dae104c6da9ed3c0efb66b85414bf9b91cd091c
Mohamed Jasim <spk@mjcomputer.local>
2025-08-09 23:09:43 +0300

    new commit

--- server.js
+++ server.js
@@ -12,7 +12,23 @@ mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://sparktadrib.onrender.com'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
 app.use(express.json());
 app.use(logger('dev'));
 
